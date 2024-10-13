2dcfb0190b8ea65fb70e92a0a1abb5b5d5947f01
NL185095 <nikolalabovicpn@gmail.com>
2024-10-13 19:31:17 +0200

    edit

--- main.tf
+++ main.tf
@@ -11,9 +11,18 @@ resource "aws_vpc" "test_vpc" {
 resource "aws_subnet" "test_subnet" {
   vpc_id = aws_vpc.test_vpc.id
   cidr_block = "10.123.1.0/24"
+  map_public_ip_on_launch = true
   availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "test"
+  }
 }
 
 resource "aws_internet_gateway" "test_gateway" {
   vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "test-igw"
+  }
 }
